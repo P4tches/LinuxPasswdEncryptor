af1fc38b1123281b90419c2c5e99fb9843ddc549
P4tches <P4tches@users.noreply.github.com>
2016-09-16 16:22:04 -0700

    Create encryptor.sh

---
+++ encryptor.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+read gPWD
+for i in $(getent passwd | grep /home/ | cut -d  ':' -f 1); do
+PWD=$(cat /dev/urandom | tr -dc 'a-zA-Z1-9' | fold -w 8 | head -n 1)
+chpasswd <<<"$i:$PWD"
+echo $i"    "$PWD | openssl enc -e -aes-256-cbc -a -salt -pass pass:$gPWD >> password.txt #change to /tmp
+done
