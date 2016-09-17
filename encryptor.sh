508a788e928e49c084c98eafdb2ba4873fc3fa4a
P4tches <P4tches@users.noreply.github.com>
2016-09-16 17:24:09 -0700

    Update encryptor.sh

--- encryptor.sh
+++ encryptor.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 read gPWD
+rm password.txt
 for i in $(getent passwd | grep /home/ | cut -d  ':' -f 1); do
 PWD=$(cat /dev/urandom | tr -dc 'a-zA-Z1-9' | fold -w 8 | head -n 1)
 chpasswd <<<"$i:$PWD"
